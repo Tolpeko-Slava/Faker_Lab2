64e77db052379e263c448b6d535283f1a49e425d
Slava <tolpeko-slava@mail.ru>
2021-11-17 12:41:03 +0300

    2LabAddGeneratorObject

---
+++ FakerLib/FakerLib/SingleObjectGenerator/FloatGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerLib.Generator
+{
+    class FloatGenerator: IGenerator
+    {
+        private Random rand = new Random(DateTime.Now.Millisecond);
+        private Type resultType = typeof(float);
+
+        public Type ResultType
+        {
+            get
+            {
+                return resultType;
+            }
+
+        }
+
+        public object Generate()
+        {
+            float result = (float)(rand.NextDouble() * rand.Next());
+
+            if (rand.Next() % 2 == 0)
+                result = -result;
+
+            return result;
+        }
+
+
+    }
+}
---
+++ FakerLib/FakerLib/SingleObjectGenerator/LongGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerLib.Generator
+{
+    class LongGenerator:IGenerator
+    {
+        private Random rnd = new Random(DateTime.Now.Millisecond);
+        private Type resultType = typeof(long);
+
+        public Type ResultType
+        {
+            get
+            {
+                return resultType;
+            }
+
+        }
+
+        public object Generate()
+        {
+            long result = rnd.Next() * rnd.Next();
+            if (rnd.Next() % 2 == 0)
+                result = -result;
+            return result;
+        }
+    }
+}
